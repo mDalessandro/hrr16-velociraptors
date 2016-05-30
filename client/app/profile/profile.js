0e7bc130657b6c0ef797a320248c1e328dcb8831
Steven Marks <stevenrobertmarks@gmail.com>
2016-05-30 16:50:25 -0500

    closes #36

---
+++ client/app/profile/profile.js
@@ -0,0 +1,33 @@
+angular.module('greenfield.profile', [])
+
+.controller('ProfileController', function ($scope, Tags, Auth, $location) {
+
+  $scope.data = {};
+  $scope.tag = {};
+
+  //User adds tag to database
+  $scope.addTag = function(){
+    Tags.addOne($scope.tag)
+    .then($scope.getUserTags())
+  }
+
+  //gets all user tags
+  $scope.getUserTags = function(){
+    Tags.getUserAll()
+    .then(function(tags){
+      $scope.data.tags = tags;
+    });
+  }
+  //runs get all user tags on app startup
+  $scope.getUserTags();
+
+
+  $scope.signout = function(){
+    Auth.signout();
+  }
+
+  if (!Auth.isAuth()){
+    $location.path('/signin');
+  }
+
+});
