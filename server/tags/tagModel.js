c4dbba7d849f86c21a42d5550935da6a13d12591
Eduard Bardají Puig <ebardaji@gmail.com>
2016-06-04 11:22:33 -0700

    closes #133 coord validation

--- server/tags/tagModel.js
+++ server/tags/tagModel.js
@@ -1,23 +1,34 @@
 var mongoose = require('mongoose');
 
 var TagSchema = new mongoose.Schema({
- tagname: {
-  type: String,
-  required: true
+  tagname: {
+    type: String,
+    required: true
   },
- username: {
-  type: String,
-  required: true
+  username: {
+    type: String,
+    required: true
   },
- lat: {
-  type: String,
-  required: true
+  lat: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (lat) {
+        return Number(lat) >= -90 && Number(lat) <= 90;
+      },
+    message: 'Latitude out of bounds, [-90, 90]'
+    }
   },
- long: {
-  type: String,
-  required: true
+  long: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (long) {
+        return Number(long) >= -90 && Number(long) <= 90;
+      },
+    message: 'Longitude out of bounds, [-180, 180]'
+    }
   }
 });
 
 module.exports = mongoose.model('tags', TagSchema);
-
